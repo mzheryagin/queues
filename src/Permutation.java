19f5f8f43b06db07365d884afdc2c1b5848d085d
mzheryagin <zheryagin@gmail.com>
2017-10-10 14:07:56 +0300

    Project structure ready

---
+++ src/Permutation.java
@@ -0,0 +1,12 @@
+/**
+ * It takes an integer k as a command-line argument; reads in a sequence of strings
+ * from standard input using StdIn.readString(); and prints exactly k of them, uniformly at random.
+ * Print each item from the sequence at most once.
+ */
+public class Permutation
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
