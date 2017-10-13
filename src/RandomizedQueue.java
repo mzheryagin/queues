b19709dee91e89e1b4892fa04a3f20feeacb7d27
mzheryagin <zheryagin@gmail.com>
2017-10-13 18:07:25 +0300

    Almost ready

--- src/RandomizedQueue.java
+++ src/RandomizedQueue.java
@@ -1,47 +1,151 @@
+import edu.princeton.cs.algs4.StdRandom;
+import java.util.Iterator;
+
 /**
  * A randomized queue is similar to a stack or queue, except that the item removed is chosen
  * uniformly at random from items in the data structure.
- * @param <Item>
+ * @param <T>
  */
-/*public class RandomizedQueue<Item> implements Iterable<Item>
-{
-    public RandomizedQueue()                 // construct an empty randomized queue
-    {
+public class RandomizedQueue<T> implements Iterable<T> {
+  private int capacity;
+  private int itemsCount;
+  T[] queue;
 
+  /**
+   * Construct an empty randomized queue
+   */
+  public RandomizedQueue() {
+    capacity = 1;
+    itemsCount = 0;
+    queue = (T[]) new Object[capacity];
+  }
+
+  private void increaseQueueSize() {
+    capacity *= 2;
+    T[] newQueue = (T[]) new Object[capacity];
+    for (int i = 0; i < itemsCount; i++) {
+      newQueue[i] = queue[i];
+    }
+    queue = newQueue;
+  }
+
+  private void decreaseQueueSize() {
+    capacity /= 2;
+    T[] newQueue = (T[]) new Object[capacity];
+    for (int i = 0; i < itemsCount; i++) {
+      newQueue[i] = queue[i];
+    }
+    queue = newQueue;
+  }
+
+  /**
+   *  is the randomized queue empty?
+   */
+  public boolean isEmpty() {
+    return itemsCount == 0;
+  }
+
+  /**
+   * Return the number of items on the randomized queue
+   */
+  public int size() {
+    return itemsCount;
+  }
+
+  /**
+   * Add the item
+   */
+  public void enqueue(T item) {
+    if (item == null) {
+      throw new java.lang.IllegalArgumentException();
+    }
+    if (itemsCount + 1 > capacity / 2) {
+      increaseQueueSize();
+    }
+    queue[itemsCount] = item;
+    itemsCount++;
+  }
+
+  /**
+   * remove and return a random item
+   */
+  public T dequeue() {
+    if (isEmpty()) {
+      throw new java.util.NoSuchElementException();
+    }
+    if (itemsCount - 1 < capacity * 0.25) {
+      decreaseQueueSize();
+    }
+    int returnItemIndex = StdRandom.uniform(itemsCount);
+    T returnItem = queue[returnItemIndex];
+
+    //replace deleted item with the last one and make it null
+    queue[returnItemIndex] = queue[--itemsCount];
+    queue[itemsCount] = null;
+    return returnItem;
+  }
+
+  /**
+   * return a random item (but do not remove it)
+   */
+  public T sample() {
+    if (itemsCount == 0) {
+      throw new java.util.NoSuchElementException();
+    }
+    return queue[StdRandom.uniform(itemsCount)];
+  }
+
+  private class QueueIterator implements Iterator<T> {
+    private int currentIndex = 0;
+    private int[] shuffledIndexes = new int[itemsCount];
+
+    public QueueIterator() {
+      for (int i = 0; i < itemsCount; i++) {
+        shuffledIndexes[i] = i;
+      }
+      StdRandom.shuffle(shuffledIndexes);
     }
 
-    public boolean isEmpty()                 // is the randomized queue empty?
-    {
-
+    public boolean hasNext() {
+      return currentIndex < itemsCount;
     }
 
-    public int size()                        // return the number of items on the randomized queue
-    {
-
+    public T next() {
+      if (!hasNext()) {
+        throw new java.util.NoSuchElementException();
+      }
+      return queue[shuffledIndexes[currentIndex++]];
     }
 
-    public void enqueue(Item item)           // add the item
-    {
-
+    public void remove() {
+      throw new java.lang.UnsupportedOperationException();
     }
+  }
 
-    public Item dequeue()                    // remove and return a random item
-    {
+  /**
+   * return an independent iterator over items in random order
+   */
+  public Iterator<T> iterator() {
+    return new QueueIterator();
+  }
 
+  /**
+   * unit testing (optional)
+   */
+  public static void main(String[] args) {
+    int N = 10;
+    RandomizedQueue<Integer> testQueue = new RandomizedQueue();
+    for (int i = 0; i < N; i++) {
+      testQueue.enqueue(StdRandom.uniform(1000));
     }
-
-    public Item sample()                     // return a random item (but do not remove it)
-    {
-
+    for (Integer i : testQueue) {
+      System.out.println(i);
     }
-
-    public Iterator<Item> iterator()         // return an independent iterator over items in random order
-    {
-
+    System.out.println();
+    for (Integer i : testQueue) {
+      System.out.println(i);
     }
+    System.out.println();
 
-    public static void main(String[] args)   // unit testing (optional)
-    {
-
-    }
-}*/
+  }
+}
